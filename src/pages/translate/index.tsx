673a895c19e152dca8ef6b66c1e38caefbf5692b
Daniil Postnik <d.postnik1@gmail.com>
2022-10-22 18:04:01 +0300

    Added initial template for translate page. Added mixins for media queries

--- src/pages/translate/index.tsx
+++ src/pages/translate/index.tsx
@@ -1,8 +1,25 @@
+import ExchangeIcon from 'assets/icons/exchange.svg';
+
+import classes from './translate.module.scss';
+
 export default function TranslatePage() {
-    return (
+  return (
     <>
-      <div>
-          translator
+      <div className={classes.page__wrapper}>
+        <div className={classes.select__wrapper}>
+          <select className={classes.select__control}>
+            <option value="en">English</option>
+          </select>
+          <img src={ExchangeIcon} alt="arrow" width={25} height={25} />
+          <select className={classes.select__control}>
+            <option value="en">English</option>
+            <option value="ru">Русский</option>
+          </select>
+        </div>
+        <div className={classes.textarea__wrapper}>
+          <textarea></textarea>
+          <textarea></textarea>
+        </div>
       </div>
     </>
   );
